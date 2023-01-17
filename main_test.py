f32f6dffdf5067c6dc0f4435015b8d783d33fcb9
Kyu Lee <kwooler@gmail.com>
2023-01-17 11:48:47 -0800

    L23

--- main_test.py
+++ main_test.py
@@ -16,18 +16,18 @@ def test_main_1():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    p = re.compile('[\w,\W]*10[\w,\W]*20[\w,\W]*30[\w,\W]*')
+    p = re.compile(r'[\w,\W]*10[\w,\W]*20[\w,\W]*30[\w,\W]*')
     res = p.match(lines[0])
     assert res != None
     print(res.group())
 
-    p = re.compile('[\w,\W]*60[\w,\W]*')
+    p = re.compile(r'[\w,\W]*60[\w,\W]*')
     res = p.match(lines[1])
     assert res != None
     print(res.group())
 
     # p = re.compile('[\w,\W]*20\.00[\w,\W]*')
-    res = re.search('[\w,\W]*20\.00[\w,\W]*', lines[2])
+    res = re.search(r'[\w,\W]*20\.00[\w,\W]*', lines[2])
     # res = p.match(lines[2])
     assert res != None
     print(res.group())
@@ -45,17 +45,17 @@ def test_main_2():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    p = re.compile('[\w,\W]*10[\w,\W]*15[\w,\W]*25[\w,\W]*')
+    p = re.compile(r'[\w,\W]*10[\w,\W]*15[\w,\W]*25[\w,\W]*')
     res = p.match(lines[0])
     assert res != None
     print(res.group())
 
-    p = re.compile('[\w,\W]*50[\w,\W]*')
+    p = re.compile(r'[\w,\W]*50[\w,\W]*')
     res = p.match(lines[1])
     assert res != None
     print(res.group())
 
-    p = re.compile('[\w,\W]*16\.67[\w,\W]*')
+    p = re.compile(r'[\w,\W]*16\.67[\w,\W]*')
     res = p.match(lines[2])
     assert res != None
     print(res.group())
