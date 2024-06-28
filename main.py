667d8604a02ac23290bf1604efeb0ea339ebb8fe
pamalasomchith <somchithpamala@gmail.com>
2024-06-28 01:40:32 -0700

    commit 1

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def main():
     total 
     average
     """
+    val1 = int(input("Enter first value: "))
+    val2 = int(input("Enter second value "))
+    val3 = int(input("Enter third value: "))
+    total = (val1 + val2 + val3)
+    average = (int(total)) / 2
+    
+    print(total)
+    print (f'Average: \t {average:.2f}')
 
     ########################################
     # Do not delete the return statement
